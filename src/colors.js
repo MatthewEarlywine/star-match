50e784184c32319d84117da2b206eaf0d7606531
matthew.earlywine <matthew.earlywine@AFSWWK47C9MT97.us.accenturefederal.com>
2022-09-22 16:39:24 -0500

    Added branch 'split-up'

---
+++ src/colors.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+// Color Theme
+const colors = {
+  available: 'lightgray',
+  used: 'lightgreen',
+  wrong: 'lightcoral',
+  candidate: 'deepskyblue',
+};
+
+export default colors;
---
+++ src/components/PlayNumber.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from 'react';
+import colors from '../colors';
+
+const PlayNumber = props => (
+  <button
+    className="number"
+    style={{backgroundColor: colors[props.status]}}
+    onClick={() => props.onClick(props.number, props.status)}
+  >
+    {props.number}
+  </button>
+);
+
+
+
+export default PlayNumber;
